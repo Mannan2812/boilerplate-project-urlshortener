b9fe0b84e40d7e6ba21f3439b322c2bb71d168bd
Mannan bansal <t-mbansal@microsoft.com>
2021-07-05 12:57:49 +0530

    Fixed validURL function

--- myApp.js
+++ myApp.js
@@ -10,16 +10,15 @@ app.use(bodyParser.urlencoded({
     extended : false
 }))
 
-function validURL(myURL) {
-    try {
-        var url = new URL(myURL)
-        return true
-    }
-    catch (_)
-    {
-        return false
-    }
- }
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
 
 app.route('/api/shorturl').post( (req, res) => {
     url_input = req.body.url
